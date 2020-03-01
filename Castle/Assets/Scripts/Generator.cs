ea25fe5997d392d395dccf6b017cc4c89d017a82
p1f123 <kr341ul@gmail.com>
2020-03-02 00:29:17 +0200

    solid layer work

--- Castle/Assets/Scripts/Generator.cs
+++ Castle/Assets/Scripts/Generator.cs
@@ -5,18 +5,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
 
 public class Generator : MonoBehaviour
 {
     public int size;
     public Tilemap back;
+    public Tilemap solid;
 
     public class Map
     {
         private char[,] background;
+        private char[,] solidTiles;
         private int size;
         private Vector2Int absoluteCoordinates;
         private Tilemap backgroundTileMap;
+        private Tilemap solidTilemap;
 
         public Vector3Int GetNewTileCoordinates(int x, int y)
         {
@@ -43,7 +47,33 @@ public class Generator : MonoBehaviour
             }
             else return new Vector3Int(-7, 2, 0);
         }
-        
+
+        public Vector3Int GetNewSolidTileCoordinates(int x, int y)
+        {
+            if (x > -1 && x < size && y > -1 && y < size)
+            {
+                switch (solidTiles[x, y])
+                {
+                    case 'r':
+                        return new Vector3Int(-7, 1, 0);
+                        break;
+                    case 'p':
+                        return new Vector3Int(-8, 0, 0);
+                        break;
+                    case 'f':
+                        return new Vector3Int(-9, 2, 0);
+                        break;
+                    case 'i':
+                        return new Vector3Int(-8, -3, 0);
+                        break;
+                    default:
+                        return new Vector3Int(-7, 2, 0);
+                        break;
+                }
+            }
+            else return new Vector3Int(-7, 2, 0);
+        }
+
         public void DrawMapByCenter(Vector2Int center)
         {
             absoluteCoordinates = center;
@@ -51,8 +81,17 @@ public class Generator : MonoBehaviour
             {
                 for (int j = -3; j < 3; j++)
                 {
-                    TileBase tile = backgroundTileMap.GetTile(GetNewTileCoordinates(absoluteCoordinates.x + i,absoluteCoordinates.y + j));
+                    TileBase tile = backgroundTileMap.GetTile(GetNewTileCoordinates(absoluteCoordinates.x + i, absoluteCoordinates.y + j));
                     backgroundTileMap.SetTile(new Vector3Int(i, j, 0), tile);
+                }
+            }
+
+            for (int i = -5; i < 6; i++)
+            {
+                for (int j = -3; j < 3; j++)
+                {
+                    TileBase tile = solidTilemap.GetTile(GetNewSolidTileCoordinates(absoluteCoordinates.x + i, absoluteCoordinates.y + j));
+                    solidTilemap.SetTile(new Vector3Int(i, j, 1), tile);
                 }
             }
         }
@@ -90,6 +129,18 @@ public class Generator : MonoBehaviour
                 }
             }
 
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    int x = Random.Range(0, 4);
+                    if (x == 0)
+                    {
+                        solidTiles[i, j] = 'r';
+                    }
+                }
+            }
+
             absoluteCoordinates = new Vector2Int(size / 2, size / 2);
         }
 
@@ -98,10 +149,12 @@ public class Generator : MonoBehaviour
             return absoluteCoordinates;
         }
 
-        public Map(int s, Tilemap b)
+        public Map(int s, Tilemap b, Tilemap solid)
         {
             background = new char[s, s];
+            solidTiles = new char[s, s];
             backgroundTileMap = b;
+            solidTilemap = solid;
             size = s;
         }
     }
@@ -111,8 +164,9 @@ public class Generator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameMap = new Map(size, back);
+        gameMap = new Map(size, back, solid);
         gameMap.Fill();
+        gameMap.DrawMapByCenter(gameMap.GetCoords());
     }
 
     // Update is called once per frame
